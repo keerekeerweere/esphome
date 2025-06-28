d91ea05dd2e2facf71165c3d8da0478a0947211c
keerekeerweere <keerekeerweere@me.compea.com>
2025-06-28 13:06:12 +0200

    spi registration handled by SPI

--- esphome/components/ethernet/__init__.py
+++ esphome/components/ethernet/__init__.py
@@ -26,6 +26,7 @@ from esphome.const import (
     CONF_POLLING_INTERVAL,
     CONF_RESET_PIN,
     CONF_SPI,
+    CONF_SPI_ID,
     CONF_CS_PIN,
     CONF_STATIC_IP,
     CONF_SUBNET,
@@ -275,7 +276,7 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     if config[CONF_TYPE] == "W5500":
-        await spi.register_spi_device(var, config)
+        #await spi.register_spi_device(var, config)
 
         cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
         if CONF_INTERRUPT_PIN in config:
